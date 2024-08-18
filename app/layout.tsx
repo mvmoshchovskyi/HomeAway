c69c0fff43e57d61e85196de486932814d8ffece
mmoshchovskyi <mykhailo.m@vivint.com>
2024-08-18 14:09:33 +0300

    Add Clerk Authorization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,15 +18,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-		<body className={inter.className}>
-		<Providers>
-			<Navbar/>
-			<main className='container py-10'>
-				{children}
-			</main>
-		</Providers>
-		</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+			<Providers>
+				<Navbar/>
+				<main className='container py-10'>
+					{children}
+				</main>
+			</Providers>
+			</body>
+			</html>
+		</ClerkProvider>
 	);
 }
