fa8329ab7b97c0a9520955262a42831659adf045
mmoshchovskyi <mykhailo.m@vivint.com>
2024-08-18 14:16:12 +0300

    Toast Component

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,6 +1,20 @@
+'use client';
+
+import { SignOutButton } from '@clerk/nextjs';
+import { useToast } from '../ui/use-toast';
+
 function SignOutLink() {
+	const { toast } = useToast();
+	const handleLogout = () => {
+		toast({ description: 'You have been signed out.' });
+	};
+
 	return (
-		<div></div>
+		<SignOutButton redirectUrl='/'>
+			<button className='w-full text-left' onClick={handleLogout}>
+				Logout
+			</button>
+		</SignOutButton>
 	);
 }
 
