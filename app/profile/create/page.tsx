08641c3aeb9acb8551656971d061156502bff704
mmoshchovskyi <mykhailo.m@vivint.com>
2024-08-19 17:53:33 +0300

    Modify Create Profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,9 +1,16 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
 import FormInput from '@/components/form/FormInput';
 import { SubmitButton } from '@/components/form/Buttons';
 import FormContainer from '@/components/form/FormContainer';
 import { createProfileAction } from '@/utils/actions';
 
-function CreateProfile() {
+async function CreateProfile() {
+	const user = await currentUser();
+
+	if(user?.privateMetadata?.hasProfile) redirect('/');
+
 	return (
 		<section>
 			<h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
