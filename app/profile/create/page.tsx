cb71f50dcaca9a8caf8b21c01c7edfafe247087c
mmoshchovskyi <mykhailo.m@vivint.com>
2024-08-19 10:57:19 +0300

    Zod createProfileAction

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,21 +1,14 @@
 import FormInput from '@/components/form/FormInput';
 import { SubmitButton } from '@/components/form/Buttons';
 import FormContainer from '@/components/form/FormContainer';
-
-const createProfileAction = async (prevState: any, formData: FormData) => {
-	'use server';
-
-	const firstName = formData.get('firstName') as string;
-	if (firstName !== 'shakeAndBake') return { message: 'there was an error...' };
-	return { message: 'Profile Created' };
-};
+import { createProfileAction } from '@/utils/actions';
 
 function CreateProfile() {
 	return (
 		<section>
 			<h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
 			<div className='border p-8 rounded-md max-w-lg'>
-				<FormContainer action={createProfileAction}>
+				<FormContainer action={ createProfileAction }>
 					<div className='grid gap-4 mt-4 '>
 						<FormInput type='text' name='firstName' label='First Name' />
 						<FormInput type='text' name='lastName' label='Last Name' />
