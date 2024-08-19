6fa96bf9384ccc40a17b450f989a9b65f2f050c9
mmoshchovskyi <mykhailo.m@vivint.com>
2024-08-19 18:25:30 +0300

    Update Profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -7,6 +7,15 @@ import { redirect } from 'next/navigation';
 
 import { profileSchema } from './schemas';
 
+export const getAuthUser =async() => {
+	const user = await currentUser();
+	if (!user) {
+		throw new Error('You must be logged in to access this route');
+	}
+	if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+	return user;
+}
+
 export const createProfileAction = async (
 	prevState: any,
 	formData: FormData
@@ -52,4 +61,41 @@ export const fetchProfileImage = async () => {
 		},
 	});
 	return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+	const user = await getAuthUser();
+
+	const profile = await db.profile.findUnique({
+		where: {
+			clerkId: user.id,
+		},
+	});
+	if (!profile) return redirect('/profile/create');
+	return profile;
+};
+
+export const updateProfileAction = async (
+	prevState: any,
+	formData: FormData
+): Promise<{ message: string }> => {
+	const user = await getAuthUser();
+	try {
+		const rawData = Object.fromEntries(formData);
+
+		const validatedFields = profileSchema.parse(rawData);
+
+		await db.profile.update({
+			where: {
+				clerkId: user.id,
+			},
+			data: validatedFields,
+		});
+		revalidatePath('/profile');
+		return { message: 'Profile updated successfully' };
+	} catch (error) {
+		return {
+			message: error instanceof Error ? error.message : 'An error occurred',
+		};
+	}
 };
