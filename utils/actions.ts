70293e6d7825516b358b41fa5a0317f26f92a6e7
Mykhailo Moshchovskyi <mykhailo.m@vivint.com>
2024-08-29 20:19:55 +0300

    createRentalAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -9,7 +9,6 @@ import { profileSchema, validateWithZodSchema, imageSchema, propertySchema } fro
 import { uploadImage } from '@/utils/supabase';
 
 const renderError = (error: unknown): { message: string } => {
-	console.log(error);
 	return {
 		message: error instanceof Error ? error.message : 'An error occurred',
 	};
@@ -130,15 +129,55 @@ export const updateProfileImageAction = async (
 };
 
 export const createPropertyAction = async (
-	prevState: any,
-	formData: FormData
+    prevState: any,
+    formData: FormData
 ): Promise<{ message: string }> => {
-	const user = await getAuthUser();
-	try {
-		const rawData = Object.fromEntries(formData);
-		const validatedFields = validateWithZodSchema(propertySchema, rawData);
-	} catch (error) {
-		return renderError(error);
-	}
-	redirect('/');
+    const user = await getAuthUser();
+    try {
+        const rawData = Object.fromEntries(formData);
+        const file = formData.get('image') as File;
+
+        const validatedFields = validateWithZodSchema(propertySchema, rawData);
+        const validatedFile = validateWithZodSchema(imageSchema, {image: file});
+        const fullPath = await uploadImage(validatedFile.image);
+
+        await db.property.create({
+            data: {
+                ...validatedFields,
+                image: fullPath,
+                profileId: user.id,
+            },
+        });
+    } catch (error) {
+        return renderError(error);
+    }
+    redirect('/');
+};
+
+
+export const fetchProperties = async ({
+    search = '',
+    category,
+}: {
+    search?: string;
+    category?: string;
+}) => {
+    const properties = await db.property.findMany({
+        where: {
+            category,
+            OR: [
+                { name: { contains: search, mode: 'insensitive' } },
+                { tagline: { contains: search, mode: 'insensitive' } },
+            ],
+        },
+        select: {
+            id: true,
+            name: true,
+            tagline: true,
+            country: true,
+            image: true,
+            price: true,
+        },
+    });
+    return properties;
 };
