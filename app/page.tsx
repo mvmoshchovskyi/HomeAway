2312b1c98ed25f82bb8a33aed55a22d7e510f49f
Mykhailo Moshchovskyi <mykhailo.m@vivint.com>
2024-08-30 10:45:30 +0300

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
-import { Button } from '@/components/ui/button';
+import CategoriesList from '@/components/home/CategoriesList';
+import PropertiesContainer from '@/components/home/PropertiesContainer';
 
-function HomePage() {
+function HomePage({
+					  searchParams,
+				  }: {
+	searchParams: { category?: string; search?: string };
+}) {
+	// console.log(searchParams);
+
 	return (
-		<>
-			<h1 className='text-3xl'>HomaPage</h1>
-			<Button variant='outline' size='lg' className='capitalize m-8'>Click me</Button>
-		</>
-	)
+		<section>
+			<CategoriesList
+				category={searchParams?.category}
+				search={searchParams?.search}
+			/>
+			<PropertiesContainer
+				category={searchParams?.category}
+				search={searchParams?.search}
+			/>
+		</section>
+	);
 }
-
 export default HomePage;
