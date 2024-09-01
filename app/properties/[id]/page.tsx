e55be0f282835d35af67a00eb5871f2ee0e15a22
Mykhailo Moshchovskyi <mykhailo.m@vivint.com>
2024-09-01 13:20:37 +0300

    Amenities

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -11,6 +11,8 @@ import UserInfo from "@/components/properties/UserInfo";
 import {Separator} from "@/components/ui/separator";
 import Description from "@/components/properties/Description";
 import Amenities from "@/components/properties/Amenities";
+import {Skeleton} from "@/components/ui/skeleton";
+import dynamic from "next/dynamic";
 
 async function PropertyDetailsPage({params}: { params: { id: string } }) {
     const property = await fetchPropertyDetails(params.id);
@@ -19,6 +21,15 @@ async function PropertyDetailsPage({params}: { params: { id: string } }) {
     const details = {baths, bedrooms, beds, guests};
     const firstName = property.profile.firstName;
     const profileImage = property.profile.profileImage;
+
+    const DynamicMap = dynamic(
+        () => import('@/components/properties/PropertyMap'),
+        {
+            ssr: false,
+            loading: () => <Skeleton className='h-[400px] w-full' />,
+        }
+    );
+
 
     return (
         <section>
@@ -42,6 +53,7 @@ async function PropertyDetailsPage({params}: { params: { id: string } }) {
                     <Separator className='mt-4' />
                     <Description description={property.description} />
                     <Amenities amenities={property.amenities} />
+                    <DynamicMap countryCode={property.country} />;
                 </div>
                 <div className='lg:col-span-4 flex flex-col items-center'>
                     <BookingCalendar/>
